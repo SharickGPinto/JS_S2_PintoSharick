e5d2648108a2332373639d4d02ee404f5f1fcdd3
Pinto <pinto702rodri@gmail.com>
2025-07-18 11:25:09 -0500

    feat :sparkless: add files

---
+++ Dia1/scrips/script.js
@@ -0,0 +1,8 @@
+//#####################
+//########DIA1#########
+//#####################
+
+
+// !!! Primer comando!!!!
+console.log("Hola Mundo!");
+//Desarrollado por: Sharick Pinto t.i 1098674201
