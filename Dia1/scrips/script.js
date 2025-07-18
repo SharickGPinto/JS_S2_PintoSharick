6e7955e0470381cd35c13cc722910caabbc3fa6c
Pinto <pinto702rodri@gmail.com>
2025-07-18 12:21:36 -0500

    feat :sparkless: add codes

--- Dia1/scrips/script.js
+++ Dia1/scrips/script.js
@@ -5,4 +5,44 @@
 
 // !!! Primer comando!!!!
 console.log("Hola Mundo!");
+variable1=3.14;
+console.log(variable1)
+console.log(typeof variable1)
 //Desarrollado por: Sharick Pinto t.i 1098674201
+//concatenacion
+palabra1= "campus";
+palabra2= "labds";
+palabra3=palabra1+palabra2;
+console.log(palabra3);
+
+nombrePersona ="Luis Angel";
+plata=5500;
+console.log("la persona llamada", nombrePersona, "tiene en su cuenta bancaria" , plata, "pesos colombianos!")
+//Desarrollado por :Pedro Gomez t.i 1098674201
+
+variable2="hola mundo";
+console.log(variable2);
+
+//Boleanos
+booleanito1= true
+booleanito2= false
+console.log(typeof booleanito1);
+//Desarrolado por Sharick 
+
+//condicionales
+if(booleanito1 == true){
+    console.log ("Es verdadero");
+}else{
+    console.log("No es verdadero :sadfeis:");
+}
+
+//Bucles
+for(i=0;i<4;i=i+1){
+    console.log("wenas");
+}
+
+holi=true;
+while(holi==true){
+        console.log("wenass");
+        holi=false;
+}
