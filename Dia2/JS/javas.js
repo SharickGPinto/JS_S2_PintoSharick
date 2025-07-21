28f4317ea3634b399dd1fcd3ee2b83c62c85d676
Pinto <pinto702rodri.com>
2025-07-21 12:39:17 -0500

    feat :sparkles: add files

---
+++ Dia2/JS/javas.js
@@ -0,0 +1,14 @@
+menu= true;
+
+while (menu){
+` =============================================
+Simulador de Gasto Diario
+=============================================
+Seleccione una opción:
+Registrar nuevo gasto
+Listar gastos
+Calcular total de gastos
+Generar reporte de gastos
+Salir
+=============================================` 
+}
