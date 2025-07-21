18b7ed4bfc264219ab7a73121ec9dee96e1fa4ba
Pinto <pinto702rodri.com>
2025-07-21 12:55:26 -0500

    feat :sparkles: add codes

--- Dia2/JS/javas.js
+++ Dia2/JS/javas.js
@@ -1,14 +1,31 @@
+//##############################
+//####SIMULADOR DE GASTOS######
+//#############################
+gasto=[]
+
 menu= true;
 
 while (menu){
-` =============================================
-Simulador de Gasto Diario
-=============================================
-Seleccione una opción:
-Registrar nuevo gasto
-Listar gastos
-Calcular total de gastos
-Generar reporte de gastos
-Salir
-=============================================` 
+    let opcion = prompt(` 
+    =============================================
+            Simulador de Gasto Diario
+    =============================================
+    1.Seleccione una opción:
+    2.Registrar nuevo gasto
+    3.Listar gastos
+    4.Calcular total de gastos
+    5.Generar reporte de gastos
+    6.Salir
+    =============================================` );
+    if (opcion ==  "1"){
+        let monto = prompt ("Monto del gasto:");
+        let categoria = prompt("Categoría (ej. comida, transporte, entretenimiento, otros)");
+        let Descripcion = prompt("Descripción (opcional)");
+        let guardar = prompt ("Ingrese 'S' para guardar o 'C' para cancelar.");
+        if (opcion == "S"){
+            
+        }
+    }
 }
+
+//Desarrollado por Sharick Giovanna t.i 1098674201
