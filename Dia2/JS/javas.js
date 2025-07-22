17b4eb373632b4ebd611230f71d0ea8d97eb560c
pinto <pinto702rodri@gmail.com~>
2025-07-21 20:49:23 -0500

    feat :sparkles: add codes

--- Dia2/JS/javas.js
+++ Dia2/JS/javas.js
@@ -1,11 +1,11 @@
 //##############################
 //####SIMULADOR DE GASTOS######
 //#############################
-gasto=[]
+gasto = []
 
-menu= true;
+menu = true;
 
-while (menu){
+while (menu) {
     let opcion = prompt(` 
     =============================================
             Simulador de Gasto Diario
@@ -17,29 +17,31 @@ while (menu){
     4.Generar reporte de gastos
     5.Salir
     =============================================` );
-    if (opcion ==  "1"){
-        let monto = prompt ("Monto del gasto:");
+    if (opcion == "1") {
+        let monto = prompt("Monto del gasto:");
         let categoria = prompt("Categoría (ej. comida, transporte, entretenimiento, otros)");
         let Descripcion = prompt("Descripción (opcional)");
-        let guardar = prompt ("Ingrese 'S' para guardar o 'N' para cancelar.");
-        if (guardar == "S"){
+        let guardar = prompt("Ingrese 'S' para guardar o 'N' para cancelar.");
+        if (guardar == "S") {
             let nuevo = {
-                "monto":monto,
-                "categoria":categoria,
-                "Descripcion":Descripcion,
+                "monto": monto,
+                "categoria": categoria,
+                "Descripcion": Descripcion,
             };
-               alert("el gasto fue guardado exitosamente");
+            alert("el gasto fue guardado exitosamente");
             gasto.push(nuevo);
-            }
-        else if (guardar == "N"){
+        }
+        else if (guardar == "N") {
             alert("NO SE GUARDO NADA");
             break;
         }
-        else {(alert("No puso nada bien, mal "));
-            break;}
+        else {
+            (alert("No puso nada bien, mal "));
+            break;
+        }
     }
-    else if (opcion == "2"){
-    let listar = prompt(`Listar Gastos
+    else if (opcion == "2") {
+        let listar = prompt(`Listar Gastos
     =============================================
     Seleccione una opción para filtrar los gastos:
 
@@ -47,19 +49,25 @@ while (menu){
     2.Filtrar por categoría
     3.Regresar al menú principal
     =============================================` );
-    if (listar ==  "1"){
-        let nlista = gasto.length;
-        for (i = 0; i<nlista; i++){
-            alert("Ver gastos")
-           "Monto: " + gastos[i]["monto"] + "\n" +
-            "Categoria: " + gastos[i]["categoria"] + "\n" +
-            "Descripcion: " + gastos[i]["Descripcion"] + "\n" 
-        };
-    };
-    else if (listar == "2")
-        
+        if (listar == "1") {
+            let nlista = gasto.length;
+            for (i = 0; i < nlista; i++) {
+                alert("Ver gastos"+
+                    "Monto: " + gasto[i]["monto"] + "\n" +
+                    "Categoria: " + gasto[i]["categoria"] + "\n" +
+                    "Descripcion: " + gasto[i]["Descripcion"] + "\n"
+                )
+            };
+        }
+        else if (listar == "2") {
+            let Fcategoria = prompt("SELECCIONE LA CATEGORIA")
 
-   
+
+        }
+
+
+
+    }
 }
 
 //Desarrollado por Sharick Giovanna t.i 1098674201
