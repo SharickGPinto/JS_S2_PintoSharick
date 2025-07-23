5e68a27efb6cb295df2af779235f91943a6e46c9
pinto <pinto702rodri@gmail.com~>
2025-07-22 20:09:17 -0500

    feat :sparkles: add codes the practica

--- practica/01.js
+++ practica/01.js
@@ -15,15 +15,16 @@ let letra = "l"
 let booleanito1 = true
 let boleanito2 = false
 let palabra = "holi"
-let suma = (4+4);
-let resta = (8-4);
-let multiplicacion = (4*4)
-let dividision = (4/2)
-let nose = (4**4)
-let porcentaje = (6%4)
+let suma = (4 + 4);
+let resta = (8 - 4);
+let multiplicacion = (4 * 4)
+let dividision = (4 / 2)
+let nose = (4 ** 4)
+let porcentaje = (6 % 4)
+let nada
 
 // 04 imprime por consola el valor de todas las variables
-console.log (numero);
+console.log(numero);
 console.log(letra);
 console.log(booleanito1)
 console.log(boleanito2);
@@ -34,3 +35,27 @@ console.log(multiplicacion);
 console.log(dividision);
 console.log(nose);
 console.log(porcentaje);
+
+// 05 imprime por consola el tipo de variables
+
+console.log(typeof numero);
+console.log(typeof letra);
+console.log(typeof booleanito1);
+console.log(typeof boleanito2);
+console.log(typeof nada);
+console.log(typeof palabra);
+console.log(typeof suma)
+console.log(typeof resta)
+
+// 06 modifica los valores de las variables por otros del mismo tipo
+
+numero = "5"
+console.log(numero);
+letra = "k"
+console.log(letra);
+palabra = "hola"
+console.log (palabra)
+suma = 5+5
+console.log (suma)
+resta = 5-2
+console.log (resta)
