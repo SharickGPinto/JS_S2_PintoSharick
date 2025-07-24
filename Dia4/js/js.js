a11259f135f4427c4b9cd57ed5567a366c92c91d
Pinto <pinto702rodri.com>
2025-07-24 11:34:47 -0500

    feat :sparkles: add files in the js

---
+++ Dia4/js/js.js
@@ -0,0 +1,65 @@
+ingredientes = [
+    {
+        "nombre": "Pan",
+        "descripcion": "Pan de hamburguesa clásico",
+        "precio": 2.5,
+        "stock": 500
+    },
+    {
+        "nombre": "Carne de res",
+        "descripcion": "Carne de res jugosa y sabrosa",
+        "precio": 8,
+        "stock": 300
+    },
+    {
+        "nombre": "Queso cheddar",
+        "descripcion": "Queso cheddar derretido",
+        "precio": 1.5,
+        "stock": 200
+    }
+]
+
+categorias = [
+    {
+        "nombre": "Clásica",
+        "descripcion": "Hamburguesas clásicas y sabrosas"
+    },
+    {
+        "nombre": "Vegetariana",
+        "descripcion": "Hamburguesas sin carne, perfectas para vegetarianos"
+    },
+    {
+        "nombre": "Gourmet",
+        "descripcion": "Hamburguesas gourmet con ingredientes premium"
+    }
+]
+
+hamburguesas = [
+    {
+        "nombre": "Clásica",
+        "descripcion": "Hamburguesas clásicas y sabrosas"
+    },
+    {
+        "nombre": "Vegetariana",
+        "descripcion": "Hamburguesas sin carne, perfectas para vegetarianos"
+    },
+    {
+        "nombre": "Gourmet",
+        "descripcion": "Hamburguesas gourmet con ingredientes premium"
+    }
+]
+
+chef = [
+    {
+        "nombre": "ChefA",
+        "especialidad": "Carnes"
+    },
+    {
+        "nombre": "ChefB",
+        "especialidad": "Cocina Vegetariana"
+    },
+    {
+        "nombre": "ChefC",
+        "especialidad": "Gourmet"
+    }
+]
