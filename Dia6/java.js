0a6ce3e0290c040d1d42c5bfe2dce552e12e63a1
Pinto <pinto702rodri.com>
2025-07-29 11:56:33 -0500

    feat :sparkles: add codes

--- Dia6/java.js
+++ Dia6/java.js
@@ -5,19 +5,21 @@ MEDIANTE XMLHttpRequest*/
 
 function buscarPersonajes (){
 
-    /* const nombre promt (ingresar el nombre del personaje)  */
-
+    const nombreUsar = prompt ("ingresar el nombre del personaje") 
     const xhr = new XMLHttpRequest(); //la importancion
-
     const url =`https://rickandmortyapi.com/api/character`
-
+    console.log(url)
     xhr.open ("GET", url, true);
     xhr.onreadystatechange = function (){
-        if (xhr.readyState ===4 && xhr.status === 200){
+        if (xhr.readyState === 3){
+            alert ("CARGANDO...")
+        }
+        else if(xhr.readyState=== 4 && xhr.status === 200){
             try{
 
                 const daticos = JSON.parse(xhr.responseText)
-                console.log (daticos)
+                alert ("el personaje es:"+daticos["results"][0]["name"]+"Status"+daticos ["results"][0]["status"]+ "Specie"+daticos["results"][0]["species"]);
+                
             }
 
             catch (err){
