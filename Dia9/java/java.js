c3d3c49ab8a6fbaaf664ec6fff03aec6b88a11bc
pinto <pinto702rodri@gmail.com~>
2025-07-30 21:09:45 -0500

    feat :sparkles: add codes fix bugs

--- Dia9/java/java.js
+++ Dia9/java/java.js
@@ -11,6 +11,30 @@ let buscarP = 1;
 const fetchP = async (pokemon) => {
     const APIPo = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
 
-    imgP.computedStyleMap.display = 'block';
-    nameP.innerHTML = DataTransfer.name;
+    if (APIPo.status == 200) {
+        const datos = await APIPo.json();
+        return datos;
+    }
 }
+const renderPK = async (pokemon) => {
+    imgP.style.display = 'none'
+    nameP.innerHTML = 'Cargando...';
+    numP.innerHTML = '';
+    const datos = await fetchP(pokemon);
+
+    if (datos) {
+        imgP.style.display = 'block';
+        nameP.innerHTML = datos.name;
+        numP.innerHTML = datos, id;
+        imgP.src = datos['sprites']['version']['generation-v']['black-white']['animated']['front_default'];
+        audP.src = datos.cries.latest;
+        input.value = '';
+        buscarP = datos.id;
+    } else {
+        imgP.style.display = 'none'
+        nameP.innerHTML = "N0 EXSTE";
+        numP.innerHTML = '';
+    }
+}
+
+form
