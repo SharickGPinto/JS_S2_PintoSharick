065a8be56ac3b614ee6cac2d3853c6ff013f2af1
pinto <pinto702rodri@gmail.com>
2025-08-01 11:07:36 -0500

    feat :sparkles: dia 8 finished

--- Dia8/XML/script.js
+++ Dia8/XML/script.js
@@ -10,40 +10,30 @@ function informacionP() {
     const nombreP = document.getElementById("nombreInput").value.trim();
     console.log(nombreP);
     const xhr = new XMLHttpRequest(); //importacion del xml
-    const url = `http https://swapi.py4e.com/api/people/1/${nombreP}`;
+    const url = `https://swapi.py4e.com/api/people/${nombreP}`;
     console.log(url);
     xhr.open("GET", url, true);
 
     xhr.onreadystatechange = function () {
-
-        if (xhr.readyState === 3) {
-
-            console.log("Cargando...");
-        }
-        else if (xhr.readyState === 4 && xhr.status === 200) {
+        if (xhr.readyState === 4 && xhr.status === 200) {
             try {
                 const datos = JSON.parse(xhr.responseText);
-                if (datos.results && datos.results.length > 0) {
-                    for (let i = 0; i < datos.results.lengthM; i++) {
-                        let division = document.getElementById("resultados");
-                        division.innerHTML += `
-                    <p>Name: <span>${datos.results[i].name}</span></p>
-                    <p>Height: <span>${datos.results[i].height}</span></p>
-                    <p>Mass: <span>${datos.results[i].mass}</span></p>
-                    <p>Hair color: <span>${datos.results[i].hair_color}</span></p>
-                    <p>Eye color: <span>${datos.results[i].eye_color}</span></p>
-                    <p>Skin color: <span>${datos.results[i].skin_color}</span></p>
-                    <p>Home World: <span>${datos.results[i].homeworld}</span></p>
-                    <p>Films: <span>${datos.results[i].films}</span></p>  
+                console.log(datos)
+                let division = document.getElementById("resultados");
+                division.innerHTML = `
+                    <p>Name: <span>${datos.name}</span></p>
+                    <p>Height: <span>${datos.height}</span></p>
+                    <p>Mass: <span>${datos.mass}</span></p>
+                    <p>Hair color: <span>${datos.hair_color}</span></p>
+                    <p>Eye color: <span>${datos.eye_color}</span></p>
+                    <p>Skin color: <span>${datos.skin_color}</span></p>
+                    <p>Home World: <span>${datos.homeworld}</span></p>
+                    <p>Films: <span>${datos.films}</span></p>  
                     `
-                        console.log(datos["results"][i]["name"]);
-                    }
-                }
-            }catch (error) {
-            console.log(err.message);
-        }
-            
-            
+                console.log(datos["name"]);
+            } catch (error) {
+                console.log("error");
+            }
         }
     };
     xhr.send();
