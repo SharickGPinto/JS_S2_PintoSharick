7fd32c91725ae2472db9a4224776f7d5aa64494c
pinto <pinto702rodri@gmail.com~>
2025-08-03 05:48:51 -0500

    feat :sparkles: create script

--- Dia10/script/script.js
+++ Dia10/script/script.js
@@ -1,6 +1,31 @@
-function cartas (){
+function cartas(done) {
     document.getElementById("cartas").innerHTML = ``;
 
-    const
+    const carticas = fetch("https://deckofcardsapi.com/static/img/back.png");
+
+    carticas
+        .then(response => response.json())
+        .then(datos => {
+            done(datos)
+
+        });
+
+    cartas(datos => {
+        datos.results.ForEach(baraja => {
+            const article = document.createRange().createContextualFragment(`
+    <article>
+        <main>
+            <div class="cartas">
+                <img src="${baraja.image}" alt="Memory Game">
+            </div>
+        </main>
+    </article>
+                `
+            );
+          const main = document.querySelector("main")
+          
+          main.append(article);
+        });
+    });
 
 }
