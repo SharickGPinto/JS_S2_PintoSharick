2dbd6fb7fedca06e2e7c51a1220295e3419dfa79
pinto <pinto702rodri@gmail.com~>
2025-08-03 16:56:29 -0500

    feat :sparkles: letters with API

--- Dia10/script/script.js
+++ Dia10/script/script.js
@@ -1,13 +1,52 @@
 /* CODIGOS DE LAS CARTAS GENERADOS MANUALMENTE */
-const rankin = ["A", "2", "3", "4", "5", "6", "7", "9", "10", "J", "Q", "K"];
+const rankin = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+const esp = ["H", "D", "C", "S"];
 
-const esp =["H", "D", "C", "S"];
-
-let cartas =[];
-
+let cartas = [];
 /*CREAR LAS 52 CARTAS */
-suits.forEach(suit => {
-    rankin.forEach(rankin => {
-        cartas.push(`${rankin}${suit}`);
+esp.forEach(suit => {
+    rankin.forEach(rank => {
+        cartas.push(`${rank}${suit}`);
     });
 });
+
+
+/* BARAJAS DE LAS CARTAS */
+function baraja(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
+/*  RENDER CARTAS VOLTEADAS */
+function renderCartas() {
+    const main = document.getElementById("cartas");
+    main.innerHTML = "";
+
+    // BARAJA DE 16 CARTAS
+    const seleccionadas = baraja([...cartas]).slice(0, 16);
+
+    seleccionadas.forEach(cartacode => {
+        const article = document.createElement("article");
+        article.classList.add("cartas")
+
+        // PARTE TRASERA DE LAS CARTAS
+        article.innerHTML = `
+        <img src="https://deckofcardsapi.com/static/img/back.png" 
+           data-front="https://deckofcardsapi.com/static/img/${cartacode}.png" 
+           alt="Carta">
+    `;
+
+        /* EVENTO DE MOVIMIENTO DE LA CARTA*/
+
+        article.addEventListener("click", () => {
+            const img = article.querySelector("img");
+            const atras = "https://deckofcardsapi.com/static/img/back.png";
+            const adelante = img.getAttribute("data-front");
+            img.src = img.src === atras ? adelante : atras;
+        });
+        main.appendChild(article);
+    });
+} 
+renderCartas();
