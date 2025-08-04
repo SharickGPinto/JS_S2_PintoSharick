53921c992215ee182d74b096ceb5ab248028de6c
pinto <pinto702rodri@gmail.com~>
2025-08-03 21:31:29 -0500

    feat :bug: buggy script

--- Dia10/script/suma.js
+++ Dia10/script/suma.js
@@ -33,4 +33,34 @@ function baraja(array) {
 
 let primeraCarta, segundaCarta, suma;
 
+function nuevaR(){
+    const cartas = document.getElementById("cartas")
+    cartas.innerHTML = "";
+    document.getElementById("resultado").textContent ="";
+    document.getElementById("respuesta").value = "";
+
+    const barajas = baraja([...cartas]) 
+    primeraCarta = barajas [0];
+    segundaCarta = barajas [1];
+    suma = valorcarta(primeraCarta)+ valorcarta(segundaCarta);
+
+    cartas.innerHTML = `
+     <img src="https://deckofcardsapi.com/static/img/${carta1}.png" alt="carta1">
+        <img src="https://deckofcardsapi.com/static/img/${carta2}.png" alt="carta2">
+    `;
+}
+
+function verificar(){
+    const respuesta = parseInt(document.getElementById("respuesya").value);
+    const resultado = document,getElementById("resultado");
+    if(respuesta === suma){
+        resultado.textContent = "win"
+    } else{
+        resultado.textContent = `mal la suma era ${suma}`;
+
+    }
+
+}
+nuevaR();
+
 
