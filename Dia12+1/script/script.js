617892af9b95f26b303e75d5997a4d46b2befb70
Pinto <pinto702rodri@gmail.com>
2025-08-05 13:02:40 -0500

    feat :bug: add codes

--- Dia12+1/script/script.js
+++ Dia12+1/script/script.js
@@ -1,32 +1,29 @@
 function informacionP() {
     document.getElementById("resultados").innerHTML = ``;
 
-    const nombreP = document.getElementById("nombreInput").value.trim();
+    const nombreP = document.getElementById("nombreInput").value;
     console.log(nombreP);
     const xhr = new XMLHttpRequest();
-    const url = `https://superheroapi.com/api/ca4c752a3aac881d5bb269f1aa297311/search/${nombreP}`;
+    const url = `https://superheroapi.com/api.php/ca4c752a3aac881d5bb269f1aa297311/search/${nombreP}`;
     console.log(url);
-    xhr.open("GET", url, true);
+    xhr.open(`GET`, url,true);
 
-    xhr.onreadystatechange = function () {
+    xhr.onreadystatechange = ()=>{
+        console.log("holi")
         if (xhr.readyState === 4 && xhr.status === 200) {
             try {
                 const datos = JSON.parse(xhr.responseText);
                 console.log(datos)
                 let division = document.getElementById("resultados");
                 division.innerHTML = `
-                <p>Name: <span>${datos.name}</span></p>
-                <p>Id: <span>${datos.id}</span></p>
-                <p>POWERS: <span>${datos.powerstats}</span></p>
-                <p>BIOGRAPHY: <span>${datos.biography}</span></p>
-                <p>APARENCE: <span>${datos.appearance}</span></p>
+                <p>Name: <span>${datos.results}</span></p>
                 `
                 console.log(datos["name"]);
             } catch (error) {
                 console.log("error");
             }
         }
-        xhr.send();
     }
+    xhr.send();
     
 }
