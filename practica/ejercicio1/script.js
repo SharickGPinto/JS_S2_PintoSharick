f5bf1133616e00cfee4ad45b2b88de134d549d6d
pinto <pinto702rodri@gmail.com~>
2025-08-04 22:42:11 -0500

    feat :sparkles: finish exercise 1

--- practica/ejercicio1/script.js
+++ practica/ejercicio1/script.js
@@ -3,32 +3,42 @@
 operaciones = true
 
 while (operaciones) {
-    
+
     let opciones = prompt(`================
     SELECCIONA LA OPERACION QUE DESEAS REALIZAR
     1. SUMA
     2. RESTA
     3. MULTIPLICACION
     4. DIVISION
+    5. Salir
     ==================`);
-    if (opciones == "1"){
-    let a =Number (prompt(`ESCRIBE EL PRIMER NUMERO` ))
+    if (opciones == "1") {
+        let a = Number(prompt(`ESCRIBE EL PRIMER NUMERO`))
 
-    let b = Number  (prompt(`Escribe el segundo numero`))
-    
-        alert(a+b)
-        
-    } else if (opciones == "2"){
-        let a =Number (prompt(`ESCRIBE EL PRIMER NUMERO` ))
+        let b = Number(prompt(`Escribe el segundo numero`))
 
-    let b = Number  (prompt(`Escribe el segundo numero`))
-    
-        alert(a-b)
-    }
+        alert(a + b)
 
-        
+    } else if (opciones == "2") {
+        let a = Number(prompt(`ESCRIBE EL PRIMER NUMERO`))
 
-    
-    
+        let b = Number(prompt(`Escribe el segundo numero`))
+
+        alert(a - b)
+    } else if (opciones == "3") {
+        let a = Number(prompt(`ESCRIBE EL PRIMER NUMERO`))
+
+        let b = Number(prompt(`Escribe el segundo numero`))
+
+        alert(a * b)
+    } else if (opciones == "4") {
+        let a = Number(prompt(`ESCRIBE EL PRIMER NUMERO`))
+
+        let b = Number(prompt(`Escribe el segundo numero`))
+
+        alert(a / b)
+    } else (opciones == "5")
+    break
+
 }
 
