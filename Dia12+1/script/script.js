f598cc3fd35132dc8d36157421bd75a5cccaefd8
Pinto <pinto702rodri@gmail.com>
2025-08-05 12:25:53 -0500

    feat :bug: add codes

--- Dia12+1/script/script.js
+++ Dia12+1/script/script.js
@@ -2,11 +2,12 @@ function informacionP() {
     document.getElementById("resultados").innerHTML = ``;
 
     const nombreP = document.getElementById("nombreInput").value.trim();
-    console.log(nombrep);
+    console.log(nombreP);
     const xhr = new XMLHttpRequest();
     const url = `https://superheroapi.com/api/ca4c752a3aac881d5bb269f1aa297311/search${nombreP}`;
     console.log(url);
     xhr.open("GET", url, true);
+
     xhr.onreadystatechange = function () {
         if (xhr.readyState === 4 && xhr.status === 200) {
             try {
@@ -15,14 +16,12 @@ function informacionP() {
                 let division = document.getElementById("resultados");
                 division.innerHTML = `
                 <p>Name: <span>${datos.name}</span></p>
-                <p>Height: <span>${datos.height}</span></p>
-                <p>Mass: <span>${datos.mass}</span></p>
-                <p>Hair color: <span>${datos.hair_color}</span></p>
-                <p>Eye color: <span>${datos.eye_color}</span></p>
-                <p>Skin color: <span>${datos.skin_color}</span></p>
-                <p>Home World: <span>${datos.homeworld}</span></p>
-                <p>Films: <span>${datos.films}</span></p>  
+                <p>Id: <span>${datos.id}</span></p>
+                <p>POWERS: <span>${datos.powerstats}</span></p>
+                <p>BIOGRAPHY: <span>${datos.biography}</span></p>
+                <p>APARENCE: <span>${datos.appearance}</span></p>
                 `
+                console.log(datos["name"]);
             }catch (error) {
                 console.log("error");
             }
