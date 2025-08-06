31e3652aa6541ee32af270788f2e486c40fbabe4
pinto <pinto702rodri@gmail.com~>
2025-08-05 20:37:59 -0500

    feat :sparkles: finish exercise

--- Dia12+1/script/script.js
+++ Dia12+1/script/script.js
@@ -6,24 +6,32 @@ function informacionP() {
     const xhr = new XMLHttpRequest();
     const url = `https://superheroapi.com/api.php/ca4c752a3aac881d5bb269f1aa297311/search/${nombreP}`;
     console.log(url);
-    xhr.open(`GET`, url,true);
+    xhr.open(`GET`, url);
 
-    xhr.onreadystatechange = ()=>{
-        console.log("holi")
+    xhr.onreadystatechange = function () {
+
         if (xhr.readyState === 4 && xhr.status === 200) {
             try {
-                const datos = JSON.parse(xhr.responseText);
+                let datos = JSON.parse(xhr.responseText);
                 console.log(datos)
-                let division = document.getElementById("resultados");
-                division.innerHTML = `
-                <p>Name: <span>${datos.results}</span></p>
-                `
-                console.log(datos["name"]);
-            } catch (error) {
+                if (datos.results && datos.results.length > 0); {
+                    for (let i = 0; 1 < datos.results.length; i++) {
+                        let division = document.getElementById("resultados");
+                        division.innerHTML = `
+                        <div class = "heroues">
+                        <img src="${datos["results"][i]["image"]["url"]}">
+                        <h3>${datos["results"][i]["name"]}</h3>
+                        <p><strong>ID:</strong>${datos["results"][i]["id"]}</p>
+                        </div>`
+                        console.log(datos);
+                    };
+            }
+            }
+            catch (error) {
                 console.log("error");
             }
         }
-    }
+    };
     xhr.send();
-    
+
 }
